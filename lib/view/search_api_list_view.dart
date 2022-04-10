166bc29e45fd424b4d47216f16d36c9294d22bc8
masal9pse <returnymgstokh@icloud.com>
2022-04-11 05:50:40 +0900

    キーワード検索フォームを作成

---
+++ lib/view/search_api_list_view.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class SearchApiListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GithubAPI検索App'),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            width: screenWidth * 0.8,
+            padding: EdgeInsets.only(top: 10),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'キーワードを入力して完了ボタンを押してください',
+                errorMaxLines: 2,
+                errorStyle: TextStyle(color: Colors.red),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 0, horizontal: 7),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                  ),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red),
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue),
+                ),
+                disabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
