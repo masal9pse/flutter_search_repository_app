779b88bc1d2a72927b7ed14863a15d9642c9ed1d
山本大翔 <masato.yamamoto@sellca-sellcar.com>
2022-12-29 01:00:26 +0900

    enumから情報を引っ張る書き方を最新バージョンに変更

--- lib/const/enum/response_enum.dart
+++ lib/const/enum/response_enum.dart
@@ -1,59 +1,15 @@
 enum ResponseEnum {
-  success,
-  invalid,
-  noConnection,
-  format,
-  notModified,
-  badRequest,
-  serverProblem,
-  timeout,
-  other,
-}
+  success(message: 'サーバーから情報を取得することに成功しました。', status: 200),
+  invalid(message: '無効な値が返却されました。', status: 100),
+  noConnection(message: 'インターネットに接続できませんでした', status: 101),
+  format(message: '無効な方式です。', status: 102),
+  notModified(message: '変更されていません。同じキーワードが入力されている可能性があります。', status: 103),
+  badRequest(message: '検証に失敗しました。時間をおいて再度実行してください。', status: 104),
+  serverProblem(message: '検証に失敗しました。時間をおいて再度実行してください。', status: 105),
+  timeout(message: 'タイムアウトしました。インターネットに接続してください。', status: 106),
+  other(message: '例外が発生しました。時間をおいて再度実行してください。', status: 107);
 
-extension ResponseEnumExt on ResponseEnum {
-  String get message {
-    switch (this) {
-      case ResponseEnum.success:
-        return 'サーバーから情報を取得することに成功しました。';
-      case ResponseEnum.invalid:
-        return '無効な値が返却されました。';
-      case ResponseEnum.noConnection:
-        return 'インターネットに接続できませんでした';
-      case ResponseEnum.format:
-        return '無効な方式です。';
-      case ResponseEnum.notModified:
-        return '変更されていません。同じキーワードが入力されている可能性があります。';
-      case ResponseEnum.badRequest:
-        return '検証に失敗しました。時間をおいて再度実行してください。';
-      case ResponseEnum.serverProblem:
-        return '検証に失敗しました。時間をおいて再度実行してください。';
-      case ResponseEnum.timeout:
-        return 'タイムアウトしました。インターネットに接続してください。';
-      case ResponseEnum.other:
-        return '例外が発生しました。時間をおいて再度実行してください。';
-    }
-  }
-
-  int get status {
-    switch (this) {
-      case ResponseEnum.success:
-        return 200;
-      case ResponseEnum.invalid:
-        return 100;
-      case ResponseEnum.noConnection:
-        return 101;
-      case ResponseEnum.format:
-        return 102;
-      case ResponseEnum.notModified:
-        return 103;
-      case ResponseEnum.badRequest:
-        return 104;
-      case ResponseEnum.serverProblem:
-        return 105;
-      case ResponseEnum.timeout:
-        return 106;
-      case ResponseEnum.other:
-        return 107;
-    }
-  }
+  const ResponseEnum({required this.message, required this.status});
+  final String message;
+  final int status;
 }
