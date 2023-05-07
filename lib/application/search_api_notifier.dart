359604827f4c84fef3a7941a71e4f189a6ec0b0f
masal9pse <returnymgstokh@icloud.com>
2023-05-07 23:34:54 +0900

    guardを使用して、try catchを使用しないようにした

--- lib/application/search_api_notifier.dart
+++ lib/application/search_api_notifier.dart
@@ -1,6 +1,4 @@
-import 'package:flutter_engineer_codecheck/infrastructure/search_api_service.dart';
 import 'package:flutter_engineer_codecheck/model/search_api_struct.dart';
-import 'package:flutter_engineer_codecheck/repository/search_api_repository.dart';
 import 'package:flutter_engineer_codecheck/repository/search_api_repository_impl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'search_api_notifier.g.dart';
@@ -12,15 +10,12 @@ class SearchApiNotifier extends _$SearchApiNotifier {
     return null;
   }
 
-  // 状態を変更したときにisRefreshingをtrueにする
   Future<void> updateState(String input) async {
-    try {
-      state = const AsyncValue.loading();
+    state = const AsyncValue.loading();
       final repository = ref.watch(searchApiRepositoryProvider);
-      final data = await repository.getApiListInfo(input: input);
-      state = AsyncValue.data(data);
-    } catch (e,stackTrace) {
-      state = AsyncValue.error(e, stackTrace);
-    }
+      state = await AsyncValue.guard(() async {
+        final data = await repository.getApiListInfo(input: input);
+        return data;
+      });
   }
 }
