93992d21bdc384031b56f2148000dd8110309582
masal9pse <returnymgstokh@icloud.com>
2023-05-21 04:41:34 +0900

    通常のローカル環境ではモックデータを検索結果として出力するようにした。

--- lib/application/const/enum/flavor.dart
+++ lib/application/const/enum/flavor.dart
@@ -1,11 +1,10 @@
 /// フレーバー
 enum Flavor {
-  /// 開発
-  dev,
-  /// 開発で本番APIを叩く
-  devStg,
-  /// 試験
-  stg,
-  /// 本番
-  prd,
+  dev(arg: 'dev'),
+  devStg(arg: 'dev_stg'),
+  stg(arg: 'stg'),
+  prd(arg: 'prd');
+
+  const Flavor({required this.arg});
+  final String arg;
 }
