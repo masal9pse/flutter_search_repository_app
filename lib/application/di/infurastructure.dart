88e04b301598e117a1211ab31ce9c1c4207ddadf
masal9pse <returnymgstokh@icloud.com>
2023-05-21 14:44:19 +0900

    モックデータをjsonファイルに変更

--- lib/application/di/infurastructure.dart
+++ lib/application/di/infurastructure.dart
@@ -3,8 +3,8 @@ import 'package:flutter_engineer_codecheck/application/const/enum/flavor.dart';
 import 'package:flutter_engineer_codecheck/damain/repository/search_api_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 // DI は特別に外レイヤーをimportする
-import 'package:flutter_engineer_codecheck/infrastructure/search_fake_api_repository.dart';
 import 'package:flutter_engineer_codecheck/infrastructure/search_github_api_repository.dart';
+import 'package:flutter_engineer_codecheck/infrastructure/search_fake_api_repository.dart';
 
 final searchApiRepositoryProvider = Provider<SearchApiRepository>(
   ((ref) {
--- lib/infrastructure/search_fake_api_repository.dart
+++ lib/infrastructure/search_fake_api_repository.dart
@@ -1,18 +1,20 @@
+import 'dart:convert';
+
 import 'package:flutter_engineer_codecheck/damain/entity/search_api_struct.dart';
 import 'package:flutter_engineer_codecheck/damain/repository/search_api_repository.dart';
 import 'package:flutter_engineer_codecheck/damain/types/error.dart';
 import 'package:flutter_engineer_codecheck/damain/types/result.dart';
-import 'package:flutter_engineer_codecheck/infrastructure/test_data/api_mock_test_data.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class SearchFakeApiRepository implements SearchApiRepository {
   @override
   Future<ApiResults> getApiListInfo({
     required String input,
-  }) {
-    final apiSuccessTestData01 = ApiMockTestData().apiSuccessTestData01;
-    final convertedApiSuccessTestData01 =
-        SearchApiModelStruct.fromJson(apiSuccessTestData01);
-    return Future.value(Result.success(convertedApiSuccessTestData01));
+  }) async {
+    final loadedData = await rootBundle.loadString('assets/json/fake_api_mock_data.json');
+    final decodedData = jsonDecode(loadedData);
+    final convertedData = SearchApiModelStruct.fromJson(decodedData);
+    return Future.value(Result.success(convertedData));
   }
 }
 
@@ -21,6 +23,6 @@ class SearchFakeErrorApiRepository implements SearchApiRepository {
   Future<ApiResults> getApiListInfo({
     required String input,
   }) {
-      return Future.value(const Result.failure(AppError.fetchError()));
+    return Future.value(const Result.failure(AppError.fetchError()));
   }
 }
