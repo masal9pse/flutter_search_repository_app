1596dfb26def9d2273423c59c1c9d5d540723764
masal9pse <returnymgstokh@icloud.com>
2023-10-23 03:05:53 +0900

    #78 go_routerの削除

--- lib/presentation/router/app.dart
+++ lib/presentation/router/app.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_engineer_codecheck/application/const/enum/page_info_enum.dart';
-import 'package:flutter_engineer_codecheck/presentation/router/go_router.dart';
+import 'package:flutter_engineer_codecheck/presentation/view/pages/search_api_list_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routeInformationProvider: router.routeInformationProvider,
-      routeInformationParser: router.routeInformationParser,
-      routerDelegate: router.routerDelegate,
-      title: PageInfoEnum.top.title,
+    return MaterialApp(      
       theme: ThemeData(
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Colors.white,
       ),
+      home: SearchApiListPage(),
     );
   }
 }
