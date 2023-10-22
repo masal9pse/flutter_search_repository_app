d98c81389be0b125e16337a7f21b5c0b9be3e36a
masal9pse <returnymgstokh@icloud.com>
2023-10-23 02:28:36 +0900

    一時コミット

--- test/widgets/search_api_list_view_test.dart
+++ test/widgets/search_api_list_view_test.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_engineer_codecheck/application/const/enum/page_info_enum.dart';
 import 'package:flutter_engineer_codecheck/application/di/infurastructure.dart';
 import 'package:flutter_engineer_codecheck/application/const/app_key_name.dart';
 import 'package:flutter_engineer_codecheck/infrastructure/search_fake_api_repository.dart';
+import 'package:flutter_engineer_codecheck/presentation/router/app.dart';
 import 'package:flutter_engineer_codecheck/presentation/view/pages/search_api_list_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -12,8 +14,8 @@ void main() {
   setUpAll(() => HttpOverrides.global = null);
   group('API一覧ページのテスト', () {
     MaterialApp testMainViewWidget() {
-      return MaterialApp(
-        home: SearchApiListPage(),
+      return const MaterialApp(
+        home: App(),
       );
     }
 
@@ -43,17 +45,45 @@ void main() {
             child: testMainViewWidget(),
           ),
         );
-        const input = 'Go language';
 
         await tester.enterText(
           find.byKey(AppKeyName.topPageSearchTextField),
-          input,
+          'Go language',
         );
         await tester.tap(
           find.byKey(AppKeyName.searchElevatedButton),
         );
         await tester.pumpAndSettle();
         expect(find.text('やまもとまさと'), findsOneWidget);
+      });
+
+      testWidgets('検索結果が反映されている状態で、その項目をタップすると詳細画面に遷移する',
+          (WidgetTester tester) async {
+        await tester.pumpWidget(
+          ProviderScope(
+            overrides: [
+              searchApiRepositoryProvider
+                  .overrideWith((ref) => SearchFakeApiRepository()),
+            ],
+            child: testMainViewWidget(),
+          ),
+        );
+        await tester.enterText(
+          find.byKey(AppKeyName.topPageSearchTextField),
+          'Go language',
+        );
+        await tester.tap(
+          find.byKey(AppKeyName.searchElevatedButton),
+        );
+
+        await tester.pumpAndSettle();
+        expect(find.text('やまもとまさと'), findsOneWidget);
+
+        await tester.tap(
+          find.byKey(AppKeyName.responseDetailCard(0)),
+        );
+        await tester.pumpAndSettle();
+        expect(find.text(PageInfoEnum.show.title), findsOneWidget);
       });
     });
 
