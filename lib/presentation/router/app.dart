eb6bf5c3dfa9ae81874a3419e6d94de5efae601f
masal9pse <returnymgstokh@icloud.com>
2023-10-24 06:36:35 +0900

    不要な改行を削除

--- lib/presentation/router/app.dart
+++ lib/presentation/router/app.dart
@@ -5,7 +5,7 @@ class App extends StatelessWidget {
   const App({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(      
+    return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Colors.white,
