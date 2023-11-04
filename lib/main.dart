8c50a85f0ff1aafae9722a05c018230c26a203cb
masal9pse <returnymgstokh@icloud.com>
2023-11-05 08:51:45 +0900

    #9 go_routerは2度目提出では導入しないのでrouterディレクトリから削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_engineer_codecheck/presentation/router/app.dart';
+import 'package:flutter_engineer_codecheck/presentation/app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
