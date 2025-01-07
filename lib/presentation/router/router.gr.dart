58839e00a38a42c36e631c0cfcf8d8af2aac9ea9
masal9pse <returnymgstokh@icloud.com>
2025-01-08 07:24:55 +0900

    #232 mypageの作成

--- lib/presentation/router/router.gr.dart
+++ lib/presentation/router/router.gr.dart
@@ -8,22 +8,28 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i3;
-import 'package:flutter/material.dart' as _i4;
+import 'package:auto_route/auto_route.dart' as _i6;
+import 'package:flutter/material.dart' as _i7;
 import 'package:flutter_engineer_codecheck/domain/model/search_api_model.dart'
-    as _i5;
+    as _i8;
 import 'package:flutter_engineer_codecheck/presentation/view/pages/api_show_page.dart'
     as _i1;
-import 'package:flutter_engineer_codecheck/presentation/view/pages/search_api_list_page.dart'
+import 'package:flutter_engineer_codecheck/presentation/view/pages/favorite_page.dart'
     as _i2;
+import 'package:flutter_engineer_codecheck/presentation/view/pages/my_page.dart'
+    as _i3;
+import 'package:flutter_engineer_codecheck/presentation/view/pages/root_page.dart'
+    as _i4;
+import 'package:flutter_engineer_codecheck/presentation/view/pages/search_api_list_page.dart'
+    as _i5;
 
 /// generated route for
 /// [_i1.ApiShowPage]
-class ApiShowRoute extends _i3.PageRouteInfo<ApiShowRouteArgs> {
+class ApiShowRoute extends _i6.PageRouteInfo<ApiShowRouteArgs> {
   ApiShowRoute({
-    _i4.Key? key,
-    required _i5.Item item,
-    List<_i3.PageRouteInfo>? children,
+    _i7.Key? key,
+    required _i8.Item item,
+    List<_i6.PageRouteInfo>? children,
   }) : super(
           ApiShowRoute.name,
           args: ApiShowRouteArgs(
@@ -35,7 +41,7 @@ class ApiShowRoute extends _i3.PageRouteInfo<ApiShowRouteArgs> {
 
   static const String name = 'ApiShowRoute';
 
-  static _i3.PageInfo page = _i3.PageInfo(
+  static _i6.PageInfo page = _i6.PageInfo(
     name,
     builder: (data) {
       final args = data.argsAs<ApiShowRouteArgs>();
@@ -53,9 +59,9 @@ class ApiShowRouteArgs {
     required this.item,
   });
 
-  final _i4.Key? key;
+  final _i7.Key? key;
 
-  final _i5.Item item;
+  final _i8.Item item;
 
   @override
   String toString() {
@@ -64,9 +70,66 @@ class ApiShowRouteArgs {
 }
 
 /// generated route for
-/// [_i2.SearchApiListPage]
-class SearchApiListRoute extends _i3.PageRouteInfo<void> {
-  const SearchApiListRoute({List<_i3.PageRouteInfo>? children})
+/// [_i2.FavoritePage]
+class FavoriteRoute extends _i6.PageRouteInfo<void> {
+  const FavoriteRoute({List<_i6.PageRouteInfo>? children})
+      : super(
+          FavoriteRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'FavoriteRoute';
+
+  static _i6.PageInfo page = _i6.PageInfo(
+    name,
+    builder: (data) {
+      return const _i2.FavoritePage();
+    },
+  );
+}
+
+/// generated route for
+/// [_i3.MyPage]
+class MyRoute extends _i6.PageRouteInfo<void> {
+  const MyRoute({List<_i6.PageRouteInfo>? children})
+      : super(
+          MyRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'MyRoute';
+
+  static _i6.PageInfo page = _i6.PageInfo(
+    name,
+    builder: (data) {
+      return const _i3.MyPage();
+    },
+  );
+}
+
+/// generated route for
+/// [_i4.RootPage]
+class RootRoute extends _i6.PageRouteInfo<void> {
+  const RootRoute({List<_i6.PageRouteInfo>? children})
+      : super(
+          RootRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'RootRoute';
+
+  static _i6.PageInfo page = _i6.PageInfo(
+    name,
+    builder: (data) {
+      return const _i4.RootPage();
+    },
+  );
+}
+
+/// generated route for
+/// [_i5.SearchApiListPage]
+class SearchApiListRoute extends _i6.PageRouteInfo<void> {
+  const SearchApiListRoute({List<_i6.PageRouteInfo>? children})
       : super(
           SearchApiListRoute.name,
           initialChildren: children,
@@ -74,10 +137,10 @@ class SearchApiListRoute extends _i3.PageRouteInfo<void> {
 
   static const String name = 'SearchApiListRoute';
 
-  static _i3.PageInfo page = _i3.PageInfo(
+  static _i6.PageInfo page = _i6.PageInfo(
     name,
     builder: (data) {
-      return const _i2.SearchApiListPage();
+      return const _i5.SearchApiListPage();
     },
   );
 }
