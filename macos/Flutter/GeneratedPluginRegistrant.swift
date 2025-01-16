b7194d0083c5be3e8077fa81279dfa2a1fd2f509
masal9pse <returnymgstokh@icloud.com>
2025-01-16 23:48:29 +0900

    #258 app_linksのダウンロード、podを1.13.1から1.16.2にして解消

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
 }
