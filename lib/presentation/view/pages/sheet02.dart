cec0c79de583ff77c72b1103c4a048cddb71eda7
masal9pse <returnymgstokh@icloud.com>
2025-02-11 23:30:40 +0900

    PageRouteBuilderを継承せずに実装

--- lib/presentation/view/pages/sheet02.dart
+++ lib/presentation/view/pages/sheet02.dart
@@ -34,11 +34,18 @@ class _DemoPageState extends State<DemoPage> {
         child: ElevatedButton(
           onPressed: () {
             Navigator.of(context).push(
-              // CustomPageRoute(
-              //   const NextPage(),
-              // ),
-              CustomPageRouteBuilder(
-                child: NextPage(),
+              PageRouteBuilder(
+                pageBuilder: (context, animation, secondaryAnimation) =>
+                    NextPage(),
+                transitionsBuilder:
+                    (context, animation, secondaryAnimation, child) {
+                  return FadeTransition(
+                    opacity: animation,
+                    child: child,
+                  );
+                },
+                transitionDuration: const Duration(milliseconds: 1000),
+                barrierColor: Colors.blue,
               ),
             );
           },
@@ -71,40 +78,6 @@ class NextPage extends StatelessWidget {
   }
 }
 
-class CustomPageRoute<T> extends PageRoute<T> {
-  CustomPageRoute(this.child);
-
-  @override
-  Color get barrierColor => Colors.red;
-
-  @override
-  String? get barrierLabel => null;
-
-  final Widget child;
-
-  // // doubleが強制される
-  @override
-  Widget buildPage(
-    BuildContext context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-  ) {
-    // ここを変えればいろんなトランジションにできるぞ
-    return FadeTransition(
-      opacity: animation,
-      child: child,
-    );
-  }
-
-  @override
-  bool get maintainState => true;
-
-  @override
-  Duration get transitionDuration => const Duration(
-        milliseconds: 1000, // ミリ秒でトランジションにかかる時間を指定
-      );
-}
-
 // PageRouteBuilderでは後ろの画面をいじるやり方がない説？
 class CustomPageRouteBuilder extends PageRouteBuilder {
   CustomPageRouteBuilder({required Widget child})
@@ -119,59 +92,7 @@ class CustomPageRouteBuilder extends PageRouteBuilder {
           },
           transitionDuration: const Duration(milliseconds: 1000),
         );
-  
+
   // @override
   // Color get barrierColor => Colors.red;
-
-  @override
-  DelegatedTransitionBuilder? get delegatedTransition => delegateTransition;
-  
-  // staticメソッドだからどこに定義しても挙動は変わらん。
-  // delegateTransitionは後ろの画面を担当していそう
-  static Widget delegateTransition(
-    BuildContext context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-    bool allowSnapshotting,
-    Widget? child,
-  ) {
-    final CurvedAnimation curvedAnimation = CurvedAnimation(
-      curve: Curves.linearToEaseOut,
-      reverseCurve: Curves.easeInToLinear,
-      parent: secondaryAnimation,
-    );
-
-    final Animation<BorderRadiusGeometry> radiusAnimation =
-        curvedAnimation.drive(
-      Tween<BorderRadiusGeometry>(
-        begin: BorderRadius.circular(0),
-        end: BorderRadius.circular(12),
-      ),
-    );    
-
-    return SlideTransition(
-      position: curvedAnimation.drive(
-        Tween<Offset>(
-          begin: Offset.zero,
-          end: const Offset(0.0, 0.07),
-        ),
-      ),
-      child: ScaleTransition(
-        scale: curvedAnimation.drive(Tween<double>(begin: 1.0, end: 1.0 - 0.0835)),
-        filterQuality: FilterQuality.medium,
-        alignment: Alignment.topCenter,
-        child: AnimatedBuilder(
-          animation: radiusAnimation,
-          child: child,
-          builder: (BuildContext context, Widget? child) {
-            return ClipRRect(
-              borderRadius: radiusAnimation.value,
-              child: child,
-            );
-          },
-        ),
-      ),
-    );
-  }
-
 }
