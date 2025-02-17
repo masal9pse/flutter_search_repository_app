3031439e1b88b79f8afaf3d11508af79851be517
masal9pse <returnymgstokh@icloud.com>
2025-02-18 00:12:22 +0900

    ネストしたアニメーションの対応

--- lib/presentation/animation/sheet02.dart
+++ lib/presentation/animation/sheet02.dart
@@ -11,22 +11,30 @@ class MyWidget extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final selected = useState(false);
+    final now = useState(0.0);
     return Scaffold(
       appBar: AppBar(title: Text('aaaaaa'),),
       body: Column(
         children: [
-          AnimatedAlign(
-            alignment: selected.value ? Alignment.center : Alignment.topLeft,
-            // alignment: selected.value ? Alignment(0.1, 0.1) : Alignment(0.5,0.5),
-            // alignment: selected.value ? Alignment(0.1, 0.1) : Alignment(0.5,0.5),
-            duration: Duration(milliseconds: 300),
-            child: ElevatedButton(
-              child: Text('aaaa'),
-              onPressed: () {
-                selected.value = !selected.value;
-              },
+          AnimatedOpacity(
+            opacity: now.value, // 0が完全に透明で、1が完全な不透明
+            duration: Duration(milliseconds: 3000),
+            child: AnimatedAlign(
+              alignment: selected.value ? Alignment.center : Alignment.topLeft,
+              // alignment: selected.value ? Alignment(0.1, 0.1) : Alignment(0.5,0.5),
+              // alignment: selected.value ? Alignment(0.1, 0.1) : Alignment(0.5,0.5),
+              duration: Duration(milliseconds: 300),
+              child: ElevatedButton(
+                child: Text('aaaa'),
+                onPressed: () {
+                  selected.value = !selected.value;
+                },
+              ),
             ),
           ),
+          ElevatedButton(onPressed: () {
+            now.value = 1.0;
+          }, child: Text('view'))
         ],
       ),
     );
