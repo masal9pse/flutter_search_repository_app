57ad4ed36d40c54bea8d400da412eb8bab84008f
masal9pse <returnymgstokh@icloud.com>
2025-06-18 22:05:46 +0900

    output hello world

--- lib/create_render_object_widget_sample.dart
+++ lib/create_render_object_widget_sample.dart
@@ -1,14 +1,42 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyCustomSingleChildRenderObject());
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: MyCustomSingleChildRenderObject(),
+      ),
+    ),
+  ));
 }
 
 class MyCustomRenderObject extends RenderBox {
-  
+  final TextPainter _textPainter = TextPainter(
+    text: const TextSpan(
+      text: 'Hello, RenderObject!',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 20,
+      ),
+    ),
+    textDirection: TextDirection.ltr,
+  );
+
+  @override
+  void performLayout() {
+    _textPainter.layout(); // テキストサイズを計算
+    size = Size(_textPainter.width, _textPainter.height); // 自身のサイズをテキストに合わせる
+  }
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    _textPainter.paint(context.canvas, offset); // テキストを描画
+  }
 }
 
-class MyCustomSingleChildRenderObject extends SingleChildRenderObjectWidget {
+class MyCustomSingleChildRenderObject extends LeafRenderObjectWidget {
   const MyCustomSingleChildRenderObject({super.key});
 
   @override
@@ -16,4 +44,3 @@ class MyCustomSingleChildRenderObject extends SingleChildRenderObjectWidget {
     return MyCustomRenderObject();
   }
 }
-
