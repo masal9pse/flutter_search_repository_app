4364d7b29b21c0b2385e089ac385f807d12b9202
masal9pse <returnymgstokh@icloud.com>
2025-07-05 23:33:13 +0900

    errorリストが出ることをチェック

---
+++ test/sample_unit_test3.dart
@@ -0,0 +1,8 @@
+// test/sanity_test.dart
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('sanity check3', () {
+    expect(2, equals(3));
+  });
+}
---
+++ test/sample_unit_test4.dart
@@ -0,0 +1,8 @@
+// test/sanity_test.dart
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('sanity check', () {
+    expect(3, equals(5));
+  });
+}
