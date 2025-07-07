a567467bc409f9877a4f4b40aef504ae1f40e1ba
masal9pse <returnymgstokh@icloud.com>
2025-07-07 21:48:57 +0900

    check comment

---
+++ test/golden_test/flutter_test_config.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:alchemist/alchemist.dart';
+import 'package:flutter/material.dart';
+
+Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  return AlchemistConfig.runWithConfig(
+    config: AlchemistConfig(
+      ciGoldensConfig: CiGoldensConfig(
+        obscureText: false,
+        renderShadows: true,
+        // theme: ThemeData(fontFamily: 'NotoSansJP'),
+      ),
+      platformGoldensConfig: const PlatformGoldensConfig(
+        enabled: false,
+      ),
+    ),
+    run: testMain,
+  );
+}
