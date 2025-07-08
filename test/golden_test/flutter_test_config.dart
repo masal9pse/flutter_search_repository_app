89cef9c3a15a5d67a99049918dd4bc50a730d558
masal9pse <returnymgstokh@icloud.com>
2025-07-08 23:13:15 +0900

    update macos

--- test/golden_test/flutter_test_config.dart
+++ test/golden_test/flutter_test_config.dart
@@ -7,12 +7,13 @@ Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   return AlchemistConfig.runWithConfig(
     config: AlchemistConfig(
       ciGoldensConfig: CiGoldensConfig(
-        obscureText: false,
-        renderShadows: true,
+        // obscureText: false,
+        // renderShadows: true,
+        enabled: false,
         // theme: ThemeData(fontFamily: 'NotoSansJP'),
       ),
       platformGoldensConfig: const PlatformGoldensConfig(
-        enabled: false,
+        // enabled: false,        
       ),
     ),
     run: testMain,
