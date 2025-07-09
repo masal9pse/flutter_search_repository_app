dc7e0d60512d700b2a19258e12df8e207f65751b
masal9pse <returnymgstokh@icloud.com>
2025-07-10 01:06:09 +0900

    update

--- lib/presentation/view/pages/my_home_page.dart
+++ lib/presentation/view/pages/my_home_page.dart
@@ -26,7 +26,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('bbbaaa'),
+            const Text('test1'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
