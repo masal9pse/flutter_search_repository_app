7a4235640f61decba2de321f54fa82e3c91a9420
masal9pse <returnymgstokh@icloud.com>
2025-07-10 01:13:27 +0900

    update

--- lib/presentation/view/pages/my_home_page.dart
+++ lib/presentation/view/pages/my_home_page.dart
@@ -20,13 +20,13 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('counter_app2'),
+        title: const Text('counter1'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('sample2'),
+            const Text('sample_app1'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
