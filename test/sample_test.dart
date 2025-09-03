cce4413f257b5be090ceb68fa409ea0da4871cc4
masal9pse <returnymgstokh@icloud.com>
2025-09-04 00:41:19 +0900

    update rules

--- test/sample_test.dart
+++ test/sample_test.dart
@@ -12,7 +12,7 @@ void main() {
         children: [
           GoldenTestScenario(
             name: 'with title',
-            child: ListTile(
+            child: const ListTile(
               title: Text('ListTile.title'),
             ),
           ),
