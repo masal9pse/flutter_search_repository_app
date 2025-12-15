1ca820db6061204a11e6b3e8b39549b560fb93c4
masal9pse <returnymgstokh@icloud.com>
2025-12-15 18:52:58 +0900

    open in androidした際に起動できない問題を解消

--- android/app/src/main/kotlin/com/example/flutter_ci_sample/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_ci_sample/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.example.flutter_engineer_codecheck
 
+import android.os.Bundle
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // デバッグ用ログ
+        Log.d("MainActivity", "onCreate called")
+        println("MainActivity: onCreate called")
+    }
+}
